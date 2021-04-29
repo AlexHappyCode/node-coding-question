2a08a3629d83090a5a47a250edd4b118d42a9c1f
AlexHappyCode <arpenacoding@gmail.com>
2021-04-28 23:33:51 -0700

    added sequelize migration to create the database tables

---
+++ migrations/20210429053149-createTables.js
@@ -0,0 +1,61 @@
+/* File: 20210429053149-createTables.js
+* Purpose: This migration inserts the tables into the database */
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('accounts', {
+      id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: true
+      }
+    });
+
+    await queryInterface.createTable('posts', {
+      id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      accountId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'accounts',
+          key: 'id'
+        }
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('post');
+    await queryInterface.dropTable('account');
+  }
+};
