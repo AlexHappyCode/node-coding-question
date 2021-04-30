dbbc9402d0dfcc56d0bada7f5b417bd4ae6a7143
AlexHappyCode <arpenacoding@gmail.com>
2021-04-30 09:40:28 -0700

    deleted some consolelogs

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ router.get('/', function(req, res, next) {
   // TODO currently loggedIn is not working because
   // it serializes only when creating a post
   let loggedIn;
-  console.log(req.accountId);
   req.accountId ? loggedIn = true : loggedIn = false;
-  console.log(loggedIn);
 
   res.render('index', { 
     title: 'my coding submission!',
